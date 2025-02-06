e6c31cb64dac5a0ca3a93f232d890c4068e31ee0
talk2nadar <talk2nadar@gmail.com>
2025-02-06 14:47:47 -0600

    Update streamlit_app.py
    python package 'requests' included to for RESR API calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st 
 from snowflake.snowpark.functions import col
 
-
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -44,3 +43,8 @@ if ingredients_list:        #executes everything under it if not empty
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+#New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
